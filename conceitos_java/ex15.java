0bd02a43c449b635e7f5b697caba9827aae07143
Xavi002 <diogomartins200214@gmail.com>
2020-10-30 09:13:19 +0000

    java fix bug

--- conceitos_java/ex15.java
+++ conceitos_java/ex15.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ex15 {
   public static void main(String[] args) {
-    
+    int num;
+
+    num = 0;
+
+    Scanner scanner = new Scanner(System.in);
+    Random gerador = new Random();
+
+    System.out.print();
+
+    gerador.nextInt(100);
+    if()
   }
 }
