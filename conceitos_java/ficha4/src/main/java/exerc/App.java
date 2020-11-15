6a5df2e6a3d57599dfdf3970c45cdd27c8712977
Xavi002 <diogomartins200214@gmail.com>
2020-11-15 19:05:57 +0000

    CGI home/about/services/faq

---
+++ conceitos_java/ficha4/src/main/java/exerc/App.java
@@ -0,0 +1,19 @@
+package ficha4.src.main.java.exerc;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ex01();
+    }
+
+    public static void ex01() {
+        int[] numbers = new Int[9];
+     
+        
+
+
+        // Elabore um programa que leia 10 números inteiros e os ordena. No final, deve mostrar o
+        // resultado ao utilizador.
+    }
+}
