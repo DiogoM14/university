b6633693a582ea248b559e5117ccb6dd978c414b
Xavi002 <diogomartins200214@gmail.com>
2020-12-04 16:00:52 +0000

    Inicio Exercicio 6

---
+++ conceitos_java/ficha6/src/main/java/ficha6_p2/App.java
@@ -0,0 +1,7 @@
+package ficha6_p2;
+
+public class App {
+  public static void main(String[] args) {
+    Pessoa pessoa1 = new Pessoa();
+  }
+}
---
+++ conceitos_java/ficha6/src/main/java/ficha6_p2/Pessoa.java
@@ -0,0 +1,14 @@
+package ficha6_p2;
+
+public class Pessoa {
+  String nome;
+  char genero;
+  int idade;
+
+  public Pessoa() {
+  }
+  
+  void falar() {
+    System.out.println("Blá, blá, blá...");
+  }
+}
