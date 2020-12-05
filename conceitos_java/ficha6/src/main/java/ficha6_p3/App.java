0ecfdc85c6a59da041130c815430007b243dcc57
Xavi002 <diogomartins200214@gmail.com>
2020-12-05 12:03:38 +0000

    Movies login page

---
+++ conceitos_java/ficha6/src/main/java/ficha6_p3/App.java
@@ -0,0 +1,7 @@
+package ficha6_p3;
+
+public class App {
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ conceitos_java/ficha6/src/main/java/ficha6_p3/Contador.java
@@ -0,0 +1,15 @@
+package ficha6_p3;
+
+public class Contador {
+  private int numero;
+
+  public Contador() {
+  }
+
+  public Contador() {
+  }
+
+  public primeiroNumero() {
+  }
+}
+
